d4fec71dd21570cab2a7844d45e82628964b1d2e
Agustin Rivera <arivera@nvidia.com>
2025-11-13 23:30:43 -0800

    [main] Refactor chess client from UV script to proper package with pyproject.toml

--- demos/chess_client.py
+++ demos/chess_client.py
@@ -1,12 +1,4 @@
-#!/usr/bin/env -S uv run --script
-
-# /// script
-# requires-python = ">=3.11"
-# dependencies = [
-#     "rich",
-# ]
-# ///
-
+#!/usr/bin/env python3
 """
 Chess client for Chess Arena server.
 
